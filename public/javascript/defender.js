40eba57b936a60e89b757be6ef2f55b9605bbb0f
Ole Michaelis <Ole.Michaelis@googlemail.com>
2012-01-06 23:11:07 +0100

    dummyimages next defender step

--- public/javascript/defender.js
+++ public/javascript/defender.js
@@ -1,7 +1,35 @@
 hk.defender = function() {
 
 	var that = this;
+	var selectedUnit = null;
 
-	
+	this.init = function() {
+		// dropzone
+		var dropzone = Crafty.e("2D, DOM, Image").
+			attr({
+				w: 160,
+				h: Crafty.viewport.height,
+				x: Crafty.viewport.width - 160,
+				y: 0
+			}).
+			image("/images/dropzone.png")
+
+	}
+
+	this.selectUnit = function(unit_name) {
+		selectedUnit = unit_name;
+	}
+
+	this.spawUnit = function() {
+
+	}
 
 }
+
+$('body').bind('crafty_loaded', function(){
+	var defender = new hk.defender();
+	defender.init();
+	$('#defender-controlls .enemy').bind('click.defender', function(event){
+		defender.selectUnit(event.currentTarget.id);
+	})
+})
