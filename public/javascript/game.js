6e8492a99f32a186869b03d60cc40f535a5b9a3d
Patrick Schmidt <batterie1@gmx.net>
2012-01-06 22:39:33 +0100

    wrapper div

--- public/javascript/game.js
+++ public/javascript/game.js
@@ -11,10 +11,21 @@ hk.Game = function() {
 
 		// background
 		var bg = Crafty.e("2D, DOM, Image").
-		attr({
-			w: Crafty.viewport.width,
-			h: Crafty.viewport.height
-			}).
-		image("/images/back_kitchen.jpg", "repeat");
+			attr({w: Crafty.viewport.width, h: Crafty.viewport.height}).
+			image("/images/back_kitchen.jpg", "repeat").
+			bind("EnterFrame", function() {
+				//
+			});
+
+		var player = Crafty.e("2D, Canvas, Animate, Collision, Color, HTML, player")
+			.attr({w: 15, h: 15, x: 43, y: Crafty.viewport.height - 20 - 15, last_x: 0, last_y: 0, move: {left: false, right: false, up: false, down: false}})
+			.color('#FF0000')
+			.css('z-index', 100)
+			.bind("KeyDown", function(e) {
+				//
+			})
+			.bind("KeyUp", function(e) {
+				//
+			});
 	}
 }
