b8b0c579d811a9f29cf8f3bebc5c4973937c0462
Patrick Schmidt <batterie1@gmx.net>
2012-01-07 01:31:51 +0100

    enemy spawn fix

--- public/javascript/defender.js
+++ public/javascript/defender.js
@@ -16,8 +16,9 @@ hk.defender = function() {
 		image("/images/dropzone.png")
 
 		$(dropzone._element).bind('click.dropzone', function(event) {
-			var x = (Crafty.viewport.width - 160) + event.offsetX;
-			var y = event.offsetY;
+			console.log(event);
+			var x = event.clientX;
+			var y = event.clientY;
 			that.spawnUnit(that.selectedUnit, x, y);
 		})
 
