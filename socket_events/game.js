24e7503ed951d149e5df51718e6d9a7ec8a6ce90
Ole Michaelis <Ole.Michaelis@googlemail.com>
2012-01-07 18:14:43 +0100

    intermediate

--- socket_events/game.js
+++ socket_events/game.js
@@ -1,21 +1,33 @@
 var game  = {
-	roles: {},
+	roles: {
+		ATTACKER: 'attacker',
+		DEFENDER: 'defender',
+		VISITOR: 'visitor'
+	},
 	users: {
 		attacker: null,
 		defender: null,
 		visitor: []
-	}
+	},
+	states: {
+		WAITING_FOR_PLAYERS: 'waiting_for_players',
+		WAITING_FOR_DEFENDER: 'waiting_for_defender',
+		FIRST_ROUND: 'first_round',
+		BACK_ROUND: 'back_round',
+		ATTACKER_WIN: 'attacker_win',
+		ATTACKER_LOST: 'attacker_lost'
+	},
+	state: game.states.WAITING_FOR_PLAYERS
 };
-game.roles.ATTACKER = 'attacker';
-game.roles.DEFENDER = 'defender';
-game.roles.VISITOR = 'visitor';
 
 function cleanupRoles() {
 	if (game.users.attacker && game.users.attacker.disconnected) {
 		game.users.attacker = null;
+		game.state = game.states.WAITING_FOR_PLAYERS
 	}
 	if (game.users.defender && game.users.defender.disconnected) {
 		game.users.defender = null;
+		game.state = game.states.WAITING_FOR_DEFENDER
 	}
 }
 
@@ -26,17 +38,17 @@ exports.events = function (socket) {
 		game.users[game.roles.ATTACKER] = socket;
 		socket.emit('role_update', {
 			role: game.roles.ATTACKER
-			})
+		})
 	} else if (!game.users[game.roles.DEFENDER]) {
 		game.users[game.roles.DEFENDER] = socket;
 		socket.emit('role_update', {
 			role: game.roles.DEFENDER
-			})
+		})
 	} else {
 		game.users[game.roles.VISITOR].push(socket);
 		socket.emit('role_update', {
 			role: game.roles.VISITOR
-			})
+		})
 	}
 
 	socket.on('disconnect', function(data) {
