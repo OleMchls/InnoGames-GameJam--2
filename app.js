f23be277482c34fa8e81af5e77e778b574e34b2e
Ole Michaelis <Ole.Michaelis@googlemail.com>
2012-01-08 18:00:07 +0100

    change port to 3000

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.configure('production', function(){
 
 app.get('/', routes.index);
 
-app.listen(3001);
+app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 
 // Socket.IO
